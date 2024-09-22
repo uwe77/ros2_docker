bb137257fd513d692a12de1db3c5571ece5da190
laipi <laipi.ee10@nycu.edu.tw>
2024-09-22 19:55:18 +0800

    modify environment.sh

--- environment.sh
+++ environment.sh
@@ -1,3 +1,5 @@
 source /opt/ros/humble/setup.bash
 source ./ros2_ws/install/setup.bash
 export ROS_DOMAIN_ID=0
+export GZ_SIM_RESOURCE_PATH=~/ros2_docker/gazebo/models/
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:~/ros2_docker/SITL_Model/Gazebo/models
