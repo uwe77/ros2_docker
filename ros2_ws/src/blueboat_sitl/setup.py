a056b3f1d74f31d03723dbdd44edcfba8f816f4a
uwe77 <uwe90711@gmail.com>
2024-10-02 20:12:48 +0800

    feat blueboat_sitl

---
+++ ros2_ws/src/blueboat_sitl/setup.py
@@ -0,0 +1,40 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'blueboat_sitl'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
+
+        (os.path.join('share', package_name, 'launch/robots'),
+            glob(os.path.join('launch/robots', '*.launch.py'))),
+
+        # (os.path.join('share', package_name, 'models'),
+        #     glob(os.path.join('models', '*'),recursive=True)),
+
+        (os.path.join('share', package_name, 'worlds'),
+            glob(os.path.join('worlds', '*.sdf'))),
+    
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
